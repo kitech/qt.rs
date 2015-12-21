b9d60ef95f22bb4e66d76aa8bd7d4992731a187c
drswinghead <drswinghead@gmail.com>
2015-12-21 23:20:12 +0800

    compile ok. allow some warn.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+// #[warn(non_snake_case)]
+// #[warn(non_camel_case_types)]
+// #[warn(unused_mut)]
+// #[warn(unused_attributes)]
+// #[warn(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(unused_mut)]
+#![allow(unused_attributes)]
+#![allow(unused_imports)]
+
 pub mod core;
 pub mod gui;
 pub mod widgets;
