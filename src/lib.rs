ce913b92d384421b1a8d154fc148e69c8afdaa2c
drswinghead <drswinghead@gmail.com>
2015-12-25 00:01:57 +0800

    re move #[link(...)] to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,12 @@
 #![allow(unused_attributes)]
 #![allow(unused_imports)]
 
+#[link(name = "Qt5Core")]
+#[link(name = "Qt5Gui")]
+#[link(name = "Qt5Widgets")]
+#[link(name = "QtInline")]
+extern {}  // 这行还是需要的
+
 pub mod core;
 pub mod gui;
 pub mod widgets;
