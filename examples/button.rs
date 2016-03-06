392efcfd91d561537ea96c7a2b0d79f9e6865c4a
quininer kel <quininer@live.com>
2016-03-06 21:12:55 +0800

    fix examples

--- examples/button.rs
+++ examples/button.rs
@@ -3,19 +3,19 @@ extern crate qt5;
 use qt5::*;
 
 fn main() {
-    widgets::QApplication::New((
+    widgets::QApplication::new((
         &mut 0,
         &mut "example".into(),
-        0
+        None
     ));
 
-    let button = widgets::QPushButton::New((
-        &core::QString::New(&String::from("Hello Qt.rs!")),
-        &widgets::QWidget::inheritFrom(0)
+    let button = widgets::QPushButton::new((
+        &core::QString::new(&String::from("Hello Qt.rs!")),
+        Some(&widgets::QWidget::inheritFrom(0))
     ));
 
     button.show(());
-//    button.pressed_1().connect(Box::new(|| println!("click!")));
+    button.pressed().connect(Box::new(|| println!("click!")) as Box<Fn()>);
 
     widgets::QApplication::exec_s(());
 }
